ae8a847907a5393bc6586aafdc0499ada68fb799
subGlitch <d.sankov@gmail.com>
2020-11-15 14:59:05 +0300

    Asteroid spawning

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using UniRx;
 using UnityEngine;
+
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+#pragma warning disable 0649
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] AsteroidView		_prefab;
+
+#pragma warning restore 0649
+
+
+	void Start()
+	{
+	    Observable
+			.Interval( TimeSpan.FromSeconds( 1 ))
+			.Subscribe( _ => Spawn() );
+	}
+
+
+	void Spawn()
+	{
+		AsteroidView asteroid		= Instantiate( _prefab );
+
+		asteroid.Init( Vector2.left );
+	}
 }
+
