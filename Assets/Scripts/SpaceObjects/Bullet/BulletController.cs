cf9b56a6e939c8209204586b38f824b05e51f5c0
subGlitch <d.sankov@gmail.com>
2020-11-16 12:32:25 +0300

    Bullet classes

---
+++ Assets/Scripts/SpaceObjects/Bullet/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UnityEngine;
+
+
+public class BulletController : ADestroyable
+{
+	BulletView	_view;
+
+
+	public BulletController( BulletView view )
+	{
+		_view		= view;
+
+		view.Collisions.Subscribe( _ => Destroy() );
+	}
+
+
+	public override void DestroySilently()
+	=>
+		GameObject.Destroy( _view.gameObject );
+
+}
+
