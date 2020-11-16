f9453300b0dcdc86d4ba08eccef820bc8558dff2
subGlitch <d.sankov@gmail.com>
2020-11-16 21:43:52 +0300

    settings

--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 [CreateAssetMenu( fileName = "Settings", menuName = "ScriptableObjects/Settings" )]
 public class Settings : ScriptableObject
 {
+	[Header( "Ship settings" )]
+	public float ShipSpeed;
+
+	[Header( "Asteroids settings" )]
+	public float AsteroidBaseSpeed;
+	public float AsteroidAddonSpeed;
+	public float AsteroidSpawnAreaExpand;
+	public float AsteroidSpawnRightShift;
+	public float AsteroidsSpawnRate;
+
+	[Header( "Bullets settings" )]
+	public float BulletsSpeed;
+
+	[Header( "Levels" )]
 	public List< LevelConfig >		Levels;
 }
 
