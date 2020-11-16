2071918a934a6cec87ea9c8e807b47e4e9267072
subGlitch <d.sankov@gmail.com>
2020-11-16 15:39:53 +0300

    map

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,8 +8,6 @@ public class GameController : MonoBehaviour
 		TechSettings();
 
 		UiControllers.Init();
-
-		LevelController.Instance.StartLevel();
 	}
 
 
---
+++ Assets/Scripts/UI/MapView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class MapView : MonoBehaviour
+{
+	public void OnLevel( int levelIndex )
+	{
+		LevelController.Instance.StartLevel();
+		
+		SetActive( false );
+	}
+
+
+	public void SetActive( bool isActive )
+	{
+		gameObject.SetActive( isActive );
+	}
+}
+
